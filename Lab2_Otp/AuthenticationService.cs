1a05381aa286b8b8d1e5fed050b9c9e2b21eb701
IanChen <ian.chen@taiwantaxi.com.tw>
2020-06-10 15:16:59 +0800

    adjust lab2.

--- Lab2_Otp/AuthenticationService.cs
+++ Lab2_Otp/AuthenticationService.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Lab2_Otp
 {
     public class AuthenticationService
@@ -5,10 +7,13 @@ namespace Lab2_Otp
         public bool IsValid(string account, string passcode)
         {
             // 根據 account 取得自訂密碼
-            var passwordFromDao = new ProfileDao().GetPassword(account);
+            var passwordFromDao = Context.GetPassword(account);
 
             // 根據 account 取得 RSA token 目前的亂數
-            var randomCode = new RsaTokenDao().GetRandom(account);
+            var seed = new Random((int)DateTime.Now.Ticks & 0x0000FFFF);
+            var result = seed.Next(0, 999999).ToString("000000");
+            Console.WriteLine("randomCode:{0}", result);
+            var randomCode = result;
 
             // 驗證傳入的 password 是否等於自訂密碼 + RSA token亂數
             var validPassword = passwordFromDao + randomCode;
